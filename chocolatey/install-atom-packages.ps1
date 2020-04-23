7baa404ccf5c86fb9717f1849f29b42d42a58f7f
Mark Collins <mark@mdcollins.net>
2020-04-23 17:38:25 +0200

    Updates

--- chocolatey/install-atom-packages.ps1
+++ chocolatey/install-atom-packages.ps1
@@ -11,6 +11,19 @@ $atom_packages = @( `
     "auto-detect-indentation", "atom-beautify", "split-diff", "minimap-split-diff", "atom-live-server" `
 )
 
+function Update-SessionEnvironment () {
+    foreach($level in "Machine","User") {
+        [Environment]::GetEnvironmentVariables($level).GetEnumerator() | % {
+            # For Path variables, append the new values, if they're not already in there
+            if($_.Name -match 'Path$') {
+                $_.Value = ($((Get-Content "Env:$($_.Name)") + ";$($_.Value)") -split ';' | Select -unique) -join ';'
+            }
+            $_
+        } | Set-Content -Path { "Env:$($_.Name)" }
+    }
+}
+
+
 Write-Host "`nInstalling Atom packages ...`n"
 
 foreach ($p in $atom_packages) { apm install $p }
