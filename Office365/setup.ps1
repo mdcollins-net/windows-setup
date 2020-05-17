68a10b6d72bfeb35ed512ffd8fb941e631d7f6e0
Mark Collins <mark@mdcollins.net>
2020-05-17 16:34:10 +0200

    updates

---
+++ Office365/setup.ps1
@@ -0,0 +1,16 @@
+
+#
+# Open PowerShell as Adminstrator and run:
+#
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://go.mdcollins.net/office-setup'))
+#
+
+$url_office_deployment_tool="https://files.mdcollins.net/OfficeDeploymentTool"
+$url_office_configuration_file="https://files.mdcollins.net/OfficeConfigurationFile"
+
+Invoke-WebRequest -Uri $url_office_deployment_tool -OutFile $output
+Invoke-WebRequest -Uri $url_office_configuration_file $output
+
+./OfficeDeploymentTool.exe /customize ./configuration.xml
+
+
